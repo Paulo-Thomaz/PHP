3bfba0d70388b420d017a30d2e0eb97244dc897a
Paulo-Thomaz <116681035+Paulo-Thomaz@users.noreply.github.com>
2023-08-01 14:42:15 -0300

    Add files via upload

---
+++ Meu_Primiro_site_PHP/PHP_1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 5 /* + 15 */ + 5;
+echo "<br>",$x;
+// o comando echo imprim na tela;
+echo "<br>","Meu Primeiro script PHP!";
+$turma2 = "Vamos desenvolver website em PHP";
+echo "<br>",$turma2;
+$txt = "Hello world!";
+$z = 5;
+$y = 10.5;
+echo "<br>",$txt," x = ",$z," y = ",$y;
+?>
+
+</body>
+</html>
